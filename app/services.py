5ea023e052ebe16ef51a08307fe7e43a515a735b
João Maranhão <joaofelipemaranhao@gmail.com>
2023-01-01 23:35:04 -0300

    feat: :sparkles: implement AsyncTokenService

--- app/services.py
+++ app/services.py
@@ -1,4 +1,5 @@
-from interfaces import SyncTokenServiceInterface
+import asyncio
+from interfaces import SyncTokenServiceInterface, AsyncTokenServiceInterface
 from entities import Credentials, User, UserToken
 
 
@@ -8,3 +9,11 @@ class SyncTokenService(SyncTokenServiceInterface):
 
     def _issue_token(self, user: User) -> UserToken:
         return UserToken("any_token")
+
+
+class AsyncTokenService(AsyncTokenServiceInterface):
+    async def _authenticate(self, credentials: Credentials) -> User:
+        return User(credentials.username)
+
+    async def _issue_token(self, user: User) -> UserToken:
+        return UserToken("any_token_ASYNC")
