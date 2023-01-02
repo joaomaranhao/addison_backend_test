e50001a36251cb24490f4a7ecab9e9016fed0167
João Maranhão <joaofelipemaranhao@gmail.com>
2023-01-01 23:35:26 -0300

    feat: :sparkles: run AsyncTokenService

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-from services import SyncTokenService
+from services import SyncTokenService, AsyncTokenService
 from entities import Credentials
+import asyncio
 
 
-def main():
+async def main():
     c = Credentials("any_username", "any_password")
+    # Sync Service
     service = SyncTokenService()
     token = service.request_token(c)
     print(token.token)
 
+    # Async Service
+    service2 = AsyncTokenService()
+    token2 = await service2.request_token(c)
+    print(token2.token)
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
