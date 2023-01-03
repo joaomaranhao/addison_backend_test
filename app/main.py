a2904e96acefea732ba95d150248359e86f16efe
João Maranhão <joaofelipemaranhao@gmail.com>
2023-01-03 16:22:49 -0300

    feat: create SimpleAsyncTokenService

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from services import SyncTokenService, AsyncTokenService
+from services import SyncTokenService, AsyncTokenService, SimpleAsyncTokenService
 from entities import Credentials
 import asyncio
 
 
 async def main():
     c = Credentials("any_username", "any_password")
+    valid_credentials = Credentials("house", "HOUSE")
     # Sync Service
     service = SyncTokenService()
     token = service.request_token(c)
@@ -14,6 +15,11 @@ async def main():
     service2 = AsyncTokenService()
     token2 = await service2.request_token(c)
     print(token2.token)
+
+    # Simple Async Token Service
+    service3 = SimpleAsyncTokenService()
+    token3 = await service3.request_token(valid_credentials)
+    print(token3.token)
 
 
 if __name__ == "__main__":
--- app/services.py
+++ app/services.py
@@ -1,4 +1,6 @@
+import random
 import asyncio
+from datetime import datetime
 from interfaces import SyncTokenServiceInterface, AsyncTokenServiceInterface
 from entities import Credentials, User, UserToken
 
@@ -17,3 +19,21 @@ class AsyncTokenService(AsyncTokenServiceInterface):
 
     async def _issue_token(self, user: User) -> UserToken:
         return UserToken("any_token_ASYNC")
+
+
+class SimpleAsyncTokenService(AsyncTokenService):
+    async def _authenticate(self, credentials: Credentials) -> User:
+        await asyncio.sleep(random.randint(0, 5))
+        if credentials.password == credentials.username.upper():
+            user = User(credentials.username)
+            return user
+        else:
+            raise Exception("Invalid username or password!")
+
+    async def _issue_token(self, user: User) -> UserToken:
+        await asyncio.sleep(random.randint(0, 5))
+        if user.user_id[0] == "A":
+            raise Exception("User ID cannot starts with 'A'")
+        formatted_token = user.user_id + "_" + str(datetime.now().isoformat())
+        user_token = UserToken(formatted_token)
+        return user_token
